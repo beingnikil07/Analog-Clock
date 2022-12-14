cf8e3af9eb7e5be612d4d4ef6300c86192e2252b
Nikhil Kumar Rana <109665731+beingnikhil07@users.noreply.github.com>
2022-12-14 11:45:32 +0530

    Theory part

---
+++ App.js
@@ -0,0 +1,40 @@
+/*
+  12 Hours ke liye aap 360 degree rotate karte ho
+   To aap 1 Hour ke liye rotate karoge 360/12 ,means 30 degree matlab 12 se 1 prr aane ke liye 30 degree 
+   rotate karna pdd rha hai hour end ko ,To Obiviously n Hours rotate karne ke liye hour end 
+  ko 30n rotate karna padega aur ha kuch rotation minute aur second end kiii wajah se 
+  bhi aayega so uss aaye rotation ko bhi hum 30n mai add krr denge 
+  
+  avv 60 minutes 30 degree ka rotation lekar aate hai to 1 minute 1/2 ka rotation
+  lekar aayenge ,to 'n' minutes lekar aayenge 'n/2' ka rotation  
+
+
+So from above theory
+    
+ 1.   formula for hour rotation= (30n +'n/2') 
+     
+     hour formule mai second ka rotation isliye nii liya kyuki wo bahut ke 
+     neglable rotate karega so usko hum krr sakte hai avoid
+    
+ deriving formula for minutes end
+   see
+        60 Min=360 degree ka rotation lekar aayenge 
+        1 minute=(360/60) =6 degree rotation lekar aayenge
+        so 
+        'n' minutes lekar aayenge 6n ka rotation so formula for minutes end will be
+  
+ 2. formula for minute rotation=6n
+                      in above formula we are avoiding second end rotation becz it will be 
+                      neglible
+
+3.formula for second end rotation
+         60 seconds =360 ka rotation lekar aayenge
+         1 second=360/60  =6 degree ka rotation layenge 
+    so
+     formula will 6n
+
+
+
+
+
+*/
